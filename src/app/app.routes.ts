2dc66fc4e036cbd964e82a7e6379e93e2495b9ef
lsegouin <lou-anne.segouin@epfedu.fr>
2025-03-11 11:14:01 +0100

    Third checkpoint passed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'movies', component: MoviesComponent}
+];
