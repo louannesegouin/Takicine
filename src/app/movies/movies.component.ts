74654def3ffacfb8d21fa0d3e4bda5047f77cf26
lsegouin <lou-anne.segouin@epfedu.fr>
2025-03-11 16:14:10 +0100

    Sixth checkpoint passed

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
-import { Observable } from 'rxjs';
 import { Movie } from '../models/movies';
 import { AsyncPipe } from '@angular/common';
 import { DatePipe } from '@angular/common';
@@ -13,8 +12,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
 })
-export class MoviesComponent {
-  private readonly moviesService = inject(MoviesService)
-  movies$: Observable<Movie[]> = this.moviesService.getMovies()
+export class MoviesComponent implements OnInit {
+  private readonly moviesService = inject(MoviesService);
+
+  movies!: Movie[];
+  ngOnInit(): void {
+    this.moviesService.getMovies().subscribe(movies => this.movies = movies);
+  }  
+
+  deleteMovie(id: number | undefined): void {
+    if (id === undefined) {
+      return;
+    }
+    this.moviesService.deleteMovie(id).subscribe(() => 
+        this.movies = this.movies.filter(film => film.id !== id)
+    );
+  }
+
 }
 
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -7,9 +7,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MoviesService {
-  deleteMovie(id: number) {
-    throw new Error('Method not implemented.');
-  }
+
   private readonly httpClient = inject(HttpClient)
   private readonly url = "http://localhost:8080/movies"
   moviesService: any;
@@ -21,6 +19,10 @@ export class MoviesService {
 
   addMovie(movie: Movie): Observable<Movie> {
     return this.httpClient.post<Movie>(this.url, movie);
+  }
+
+  deleteMovie(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.url}/${id}`);
   }
 
 }
